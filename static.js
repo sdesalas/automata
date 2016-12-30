f442b634811ba8a9ee26b00cc7c6db6bfdcf03c3
Steven de Salas <steven@desalasworks.com>
2016-12-30 23:50:25 +1100

    Minor fixes

--- static.js
+++ static.js
@@ -2,11 +2,17 @@ const Api = require('./lib/Api.js');
 const fs = require('fs');
 const opn = require('opn');
 
+var memory = '{}';
+var devices = '{}';
+
+try { memory = fs.readFileSync('data/memory.json'); } catch (e) {};
+try { devices = fs.readFileSync('data/devices.json'); } catch (e) {};
+
 console.log('Spinning up static site..');
 var api = new Api({
     state: {
-        memory: JSON.parse(fs.readFileSync('data/memory.json')),
-        devices: JSON.parse(fs.readFileSync('data/devices.json'))
+        memory: JSON.parse(memory),
+        devices: JSON.parse(devices)
     },
     listeners: {
         ready: function() {
